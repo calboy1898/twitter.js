8ea430e11a83ab4e70614046f587486a6095f331
Ryan Bronz <ryanbronz@Ryans-MacBook-Pro.local>
2015-03-11 19:24:50 -0400

    after live test

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 var express=require("express");
 var morgan=require("morgan");
+var swig=require("swig");
 var app=express();
+var routes=require("./routes/");
+app.use("/",routes);
+app.use(express.static(__dirname + '/public'));
+//var _=require("underscore");
 //var logger=morgan("dev");
 app.use(morgan('dev'));
+app.engine("html", swig.renderFile);
+app.set("view engine","html");
+app.set("views",__dirname + '/views');
+swig.setDefaults({ cache: false });
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
-app.get('/blah', function (req, res) {
-  res.send('Hello Blah!');
-});
+// app.get('/', function (req, res) {
+//   //res.send('Hello World!');
+//   var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+
+// });
+
+// app.get('/blah', function (req, res) {
+//   res.send('Hello Blah!');
+// });
 
 /*var server = */app.listen(3000, function () {
 
